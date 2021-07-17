77ad4c191d55638e0b5389b5da231c74cbf0a020
Amirali Esfandiari <amiralinull@gmail.com>
2021-07-17 22:14:36 +0430

    add flags for port and baud

--- kahoo.go
+++ kahoo.go
@@ -2,29 +2,29 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
-	"strconv"
 
 	"github.com/spf13/cobra"
 	"github.com/tarm/serial"
 )
+
+const defaultPort = "/dev/ttyUSB0"
+const defaultBaud = 115200
 
 var rootCmd = &cobra.Command{
 	Use: "",
 }
 
 var readCmd = &cobra.Command{
-	Use:   "read [port] [baud]",
+	Use:   "read -p [port] -b [baud]",
 	Short: "Read data from serial",
 	Long:  "Read data form serial on specific port and baud",
 	Run: func(cmd *cobra.Command, args []string) {
-		port := args[0]
-		baud, err := strconv.Atoi(args[1])
-		if err != nil {
-			panic(err)
-		}
+		port, _ := cmd.Flags().GetString("port")
+		baud, _ := cmd.Flags().GetInt("baud")
 
 		config := &serial.Config{
 			Name: port,
@@ -49,28 +49,23 @@ var readCmd = &cobra.Command{
 }
 
 var writeCmd = &cobra.Command{
-	Use:   "write [port] [baud]",
+	Use:   "write -p [port] -b [baud]",
 	Short: "Write bytes on serial",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
+		port, _ := cmd.Flags().GetString("port")
+		baud, _ := cmd.Flags().GetInt("baud")
+
 		reader := bufio.NewReader(os.Stdin)
 
-		var message string
+		var message []byte
 		for {
-			line, err := reader.ReadString('\n')
+			line, err := reader.ReadBytes('\n')
 			if err == io.EOF {
-				return
+				break
 			}
 
-			message = message + line + "\r\n"
-		}
-
-		bytes := []byte(message)
-
-		port := args[0]
-		baud, err := strconv.Atoi(args[1])
-		if err != nil {
-			panic(err)
+			message = append(message, line[:]...)
 		}
 
 		config := &serial.Config{
@@ -83,7 +78,7 @@ var writeCmd = &cobra.Command{
 			panic(err)
 		}
 
-		size, err := serialConnection.Write(bytes)
+		size, err := serialConnection.Write(message)
 		if err != nil {
 			panic(err)
 		}
@@ -92,7 +87,17 @@ var writeCmd = &cobra.Command{
 }
 
 func main() {
+
+	flag.Parse()
+
 	rootCmd.AddCommand(readCmd, writeCmd)
+
+	readCmd.Flags().StringP("port", "p", defaultPort, "Port path")
+	readCmd.Flags().IntP("baud", "b", defaultBaud, "Baud rate")
+
+	writeCmd.Flags().StringP("port", "p", defaultPort, "Port path")
+	writeCmd.Flags().IntP("baud", "b", defaultBaud, "Baud rate")
+
 	err := rootCmd.Execute()
 	if err != nil {
 		panic(err)
